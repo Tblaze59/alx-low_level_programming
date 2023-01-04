a28cbb60d61fbb3f78e2001b9ed634ea7f3e2256
Akinbo olutayo <akinbotee@gmail.com>
2023-01-04 05:53:07 -0800

    3. You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,10 @@
  */
 int factorial(int n)
 {
-	int factorial = n;
 
 	if (n < 0)
 		return (-1);
-
-	else if (n >= 0 && n <= 1)
-		return (1);
+	if (n == 0 || n == 1)
+		return(1);
+	return (n * factorial(n -1));
 }
