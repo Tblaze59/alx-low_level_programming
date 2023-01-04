41315c7bf787216b3d997bb57f4d12d437dff51d
Akinbo olutayo <akinbotee@gmail.com>
2023-01-04 05:34:15 -0800

    2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - factorial of a number
+ * @n: number whose factorial is being found
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	int factorial = n;
+
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+}
